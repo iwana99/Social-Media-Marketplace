4e26bad621b1879ad2347df6450c917fd874092f
Ivana Pavlović <iwana994994@gmail.com>
2026-07-01 17:09:08 +0200

    install vite and tailwind setup

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl text-center mt-10 text-blue-500">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
